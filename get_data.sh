2ba0f634201c6c6d2445d281b8652583abb9d136
Anish Acharya <anishacharya@utexas.edu>
2020-04-12 17:02:29 -0500

    +

--- get_data.sh
+++ get_data.sh
@@ -4,10 +4,20 @@ data_dir=$curr_dir'/data'
 
 mkdir -p "$data_dir"
 
-wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/epsilon_normalized.bz2
-wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_test.binary.bz2
+if [ -f "$data_dir"'/epsilon_normalized.bz2' ]; then echo "epsilon exists skipping download"
+else
+	wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/epsilon_normalized.bz2
+fi
+
+if [ -f "$data_dir"'/rcv1_test.binary.bz2' ]; then echo "rcv1 exists skipping download"
+else
+	wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_test.binary.bz2
+fi
 
 mv 'epsilon_normalized.bz2' "$data_dir"
 mv 'rcv1_test.binary.bz2' "$data_dir"
 
-python pickle_datasets.py
+echo "Processing epsilon"
+python3 get_data_helper.py --i "$data_dir"'/epsilon_normalized.bz2' --o "$data_dir"'/epsilon_normalized.pickle'
+echo "Processing rcv1"
+python3 get_data_helper.py --i "$data_dir"'/rcv1_test.binary.bz2' --o "$data_dir"'/rcv1_test.pickle'
---
+++ get_data_helper.py
@@ -0,0 +1,27 @@
+import pickle
+import argparse
+from sklearn.datasets import load_svmlight_file
+import time
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='get_data_helper.py')
+    parser.add_argument('--i', type=str, default='data input file',
+                        help='Pass data-set path')
+    parser.add_argument('--o', type=str, default='data pickle dump file',
+                        help='Pass output file path')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    print(args)
+    input_file = args.i
+    output_file = args.o
+    t0 = time.time()
+    A, y = load_svmlight_file(f=input_file)
+    print("Loading SVMLight File took {} sec.".format(time.time()-t0))
+    print("The file is huge hence caching into a pickle file for future use")
+    A = A.toarray()
+    with open(output_file, 'wb') as pickle_file:
+        pickle.dump((A, y), pickle_file, protocol=4)
