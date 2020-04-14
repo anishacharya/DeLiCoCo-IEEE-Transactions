81b74a07dd2202a47d0f3eeeb293d563b9bb9332
Anish Acharya <anishacharya@utexas.edu>
2020-04-13 19:35:57 -0500

    +

--- example/get_data.sh
+++ example/get_data.sh
@@ -4,22 +4,29 @@ data_dir=$curr_dir'/data'
 
 mkdir -p "$data_dir"
 
-if [ -f "$data_dir"'/epsilon_normalized.pickle' ]; then echo "epsilon exists skipping download"
-else
-	wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/epsilon_normalized.bz2
-	mv 'epsilon_normalized.bz2' "$data_dir"
-	echo "Processing epsilon"
-	python3 get_data_helper.py --i "$data_dir"'/epsilon_normalized.bz2' --o "$data_dir"'/epsilon_normalized.pickle'
+datasets=${1-epsilon}
+
+if [ "$datasets" == 'epsilon' ] || [ "$datasets" == 'all' ];
+then
+	if [ -f "$data_dir"'/epsilon_normalized.pickle' ]; then echo "epsilon exists skipping download"
+	else
+		wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/epsilon_normalized.bz2
+		mv 'epsilon_normalized.bz2' "$data_dir"
+		echo "Processing epsilon"
+		python3 get_data_helper.py --i "$data_dir"'/epsilon_normalized.bz2' --o "$data_dir"'/epsilon_normalized.pickle'
+	fi
 fi
 
-if [ -f "$data_dir"'/rcv1_test.binary.pickle' ]; then echo "rcv1 exists skipping download"
-else
-	wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_test.binary.bz2
-	mv 'rcv1_test.binary.bz2' "$data_dir"
-	echo "Processing rcv1"
-	python3 get_data_helper.py --i "$data_dir"'/rcv1_test.binary.bz2' --o "$data_dir"'/rcv1_test.pickle'
+if [ "$datasets" == 'rcv1' ] || [ "$datasets" == 'all' ];
+then
+	if [ -f "$data_dir"'/rcv1_test.binary.pickle' ]; then echo "rcv1 exists skipping download"
+	else
+		wget -t inf https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/rcv1_test.binary.bz2
+		mv 'rcv1_test.binary.bz2' "$data_dir"
+		echo "Processing rcv1"
+		python3 get_data_helper.py --i "$data_dir"'/rcv1_test.binary.bz2' --o "$data_dir"'/rcv1_test.pickle'
+	fi
 fi
-
 
 
 
