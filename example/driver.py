f5ba694bde6228e0db14eb684ba73539ff9f66f2
Anish Acharya <anishacharya@utexas.edu>
2020-04-15 07:29:17 -0500

    +

--- example/driver.py
+++ example/driver.py
@@ -13,22 +13,30 @@ def _parse_args():
                         help='Pass data-set')
     parser.add_argument('--r', type=str, default=os.path.join(curr_dir, './data/'),
                         help='Pass data root')
+    parser.add_argument('n_proc', type=int, default=5)
+
     args = parser.parse_args()
     return args
-
-
-def run_experiment():
-    raise NotImplementedError
 
 
 if __name__ == '__main__':
     args = _parse_args()
     print(args)
-    print("load data-set")
     data_set = args.d
     root = args.r
 
+    """ 
+    Get Data specifically,  AX = Y , 
+    A [samples x Feature] 
+    X parameters to estimate 
+    Y Target
+    """
+    print("loading data-set")
     t0 = time.time()
     data_reader = DataReader(root=root, data_set=data_set, download=True)
     print('Time taken to load Data {} sec'.format(time.time() - t0))
+
+    """ Run Experiment """
+
+
 
