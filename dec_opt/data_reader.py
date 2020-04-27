c14ca12f06db092ece2ecfe38f9b24cb66a20989
Anish Acharya <anishacharya@utexas.edu>
2020-04-27 10:15:24 -0500

    cifar mnist breast cancer all working

--- dec_opt/data_reader.py
+++ dec_opt/data_reader.py
@@ -1,5 +1,4 @@
 import torchvision.datasets as datasets
-from torchvision import transforms
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 from sklearn.utils import shuffle
@@ -20,6 +19,8 @@ class DataReader:
         self.download = download
         if data_set == 'mnist':
             self.A_train, self.y_train, self.A_test, self.y_test = self._get_mnist()
+        elif data_set == 'cifar10':
+            self.A_train, self.y_train, self.A_test, self.y_test = self._get_cifar10()
         elif data_set == 'breast_cancer':
             self.A_train, self.y_train, self.A_test, self.y_test = self._get_breast_cancer(test_split=test_split)
         else:
@@ -29,11 +30,14 @@ class DataReader:
         mnist_train = datasets.MNIST(root=self.root, download=self.download, train=True)
         mnist_test = datasets.MNIST(root=self.root, download=self.download, train=False)
 
-        x_train = mnist_train.train_data.numpy()
+        x_train = mnist_train.train_data.numpy()/255.0
+        x_test = mnist_test.test_data.numpy()/255.0
         y_train = mnist_train.train_labels.numpy()
-
-        x_test = mnist_test.test_data.numpy()
         y_test = mnist_test.test_labels.numpy()
+
+        # flatten the images
+        x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
+        x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
 
         # convert to binary labels
         y_train[y_train < 5] = 0
@@ -57,23 +61,35 @@ class DataReader:
         cifar10_train = datasets.CIFAR10(root='./data', download=self.download, train=True)
         cifar10_test = datasets.CIFAR10(root='./data', download=self.download, train=False)
 
-        cifar10_train_data = cifar10_train.train_data
+        x_train = cifar10_train.data/255.0
+        x_test = cifar10_test.data/255.0
+
+        y_train = np.asarray(cifar10_train.targets)
+        y_test = np.asarray(cifar10_test.targets)
+
+        y_train = y_train.reshape(y_train.shape[0], 1)
+        y_test = y_test.reshape(y_test.shape[0], 1)
+
+        # convert to binary labels
+        y_train[y_train < 5] = 0
+        y_train[y_train >= 5] = 1
+        y_test[y_test < 5] = 0
+        y_test[y_test >= 5] = 1
+
         # Convert to gray-scale
-        cifar10_train_data_gray_sc = (cifar10_train_data[:, :, :, 0] +
-                                      cifar10_train_data[:, :, :, 1] + cifar10_train_data[:, :, :, 2])/3.0
+        x_train = (x_train[:, :, :, 0] + x_train[:, :, :, 1] + x_train[:, :, :, 2])/3.0
+        x_test = (x_test[:, :, :, 0] + x_test[:, :, :, 1] + x_test[:, :, :, 2])/3.0
+        x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
+        x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
 
-        cifar10_test_data = cifar10_test.test_data
-        # Convert to gray-scale
-        cifar10_test_data_gray_sc = (cifar10_test_data[:, :, :, 0] + cifar10_test_data[:, :, :, 1] +
-                                     cifar10_test_data[:, :, :, 2])/3.0
+        # Now add the Bias term - Add a Fake dim of all 1s to the parameters
+        x_train_aug = np.ones((x_train.shape[0], x_train.shape[1] + 1))
+        x_train_aug[:, 0:x_train.shape[1]] = x_train
 
-        x_train = cifar10_train_data_gray_sc.reshape(50000, 1024).astype(np.float32)
-        y_train = np.asarray(cifar10_train.train_labels, dtype=np.float32).reshape(50000, 1)
+        x_test_aug = np.ones((x_test.shape[0], x_test.shape[1] + 1))
+        x_test_aug[:, 0:x_test.shape[1]] = x_test
 
-        x_test = cifar10_test_data_gray_sc.reshape(10000, 1024).astype(np.float32)
-        y_test = np.asarray(cifar10_test.test_labels, dtype=np.float32).reshape(10000, 1)
-        
-        return x_train, y_train, x_test, y_test
+        return x_train_aug, y_train, x_test_aug, y_test
         
     @staticmethod
     def _get_breast_cancer(test_split):
--- example/driver.py
+++ example/driver.py
@@ -29,7 +29,7 @@ def _parse_args():
 
     parser.add_argument('--topology', type=str, default='ring')
     parser.add_argument('--Q', type=int, default=3)
-    parser.add_argument('--consensus_lr', type=float, default=0.2)
+    parser.add_argument('--consensus_lr', type=float, default=0.1)
 
     parser.add_argument('--quantization_function', type=str, default='top')
     parser.add_argument('--num_levels', type=int, default=10)
