b49408403709f78bbd589514a9242ed8f6999dcb
Rudrajit Das <rudrajit1503@gmail.com>
2020-04-27 20:05:58 -0500

    Update driver.py

--- example/driver.py
+++ example/driver.py
@@ -34,6 +34,7 @@ def _parse_args():
     parser.add_argument('--quantization_function', type=str, default='top')
     parser.add_argument('--num_levels', type=int, default=10)
     parser.add_argument('--coordinates_to_keep', type=int, default=5)
+    parser.add_argument('--dropout_p', type=float, default=0.5)
 
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--lr_type', type=str, default='constant')
