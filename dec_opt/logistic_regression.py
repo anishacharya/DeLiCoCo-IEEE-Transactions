62b05a916a8e58b13e4b87c8b96d645686ce6c38
Rudrajit Das <rudrajit1503@gmail.com>
2020-04-28 18:00:26 -0500

    Update logistic_regression.py

--- dec_opt/logistic_regression.py
+++ dec_opt/logistic_regression.py
@@ -45,6 +45,17 @@ class LogisticRegression:
         logits = A @ x
         pred = self.sigmoid(logits)
         return pred
+    
+    def predict_local(self, A, machine):
+        """
+          Returns 1D array of probabilities
+          that the class label == 1
+        """
+        x = self.x_estimate[:, machine]
+
+        logits = A @ x
+        pred = self.sigmoid(logits)
+        return pred
 
     @staticmethod
     def decision_boundary(prob):
@@ -86,7 +97,7 @@ class LogisticRegression:
             N = sliced_A.shape[0]
 
             # Get Predictions
-            predictions = self.predict(A=sliced_A)
+            predictions = self.predict_local(A=sliced_A, machine=machine)
             predictions = predictions.reshape(predictions.shape[0], 1)
             gradient = np.dot(sliced_A.T, predictions - sliced_y)
             gradient /= N
