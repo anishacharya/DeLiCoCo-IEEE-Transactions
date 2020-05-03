b97c645babd0a5fb230c768f6f6d3ef1151a4458
Anish Acharya <anishacharya@utexas.edu>
2020-05-03 08:04:18 -0500

    +

--- example/plots.py
+++ example/plots.py
@@ -6,10 +6,12 @@ import itertools
 import pickle
 
 
-def plot_results(repeats, label, plot='train'):
+def plot_results(repeats, label, plot='train', optimal=0.0):
     scores = []
     for result in repeats:
         loss_val = result[0] if plot == 'train' else result[1]
+        # Get Sub Optimal Loss
+        loss_val = loss_val - optimal
         scores += [np.log(loss_val)]
 
     # scores = np.array(scores)
@@ -30,16 +32,22 @@ def plot_loop(data_set: str, algorithm: List[str], n_cores: List[int],
               topology: List[str], Q: List[int], consensus_lr: List[float],
               quantization_func: List[str],
               label: List[str]):
+
+    # Get Optimal Values
+    baselines = {'mnist': 0.35247397975085026}
+    optimal = baselines[data_set]
+
+    # Load Hyper Parameters
     all_hyper_param = list(itertools.product(algorithm, n_cores, topology,
                                              Q, consensus_lr, quantization_func))
+    # Load Data
     data = unpickle_dir(d='./results/' + data_set)
-    baselines = {'mnist': 0.35247397975085026}
-    print('Loaded Data')
+    # Generate Plots
     i = 0
     for hyper_param in all_hyper_param:
         result_file = hyper_param[0] + '.' + str(hyper_param[1]) + '.' + hyper_param[2] + \
                       '.' + str(hyper_param[3]) + '.' + str(hyper_param[4]) + '.' + hyper_param[5]
-        plot_results(repeats=data[result_file], label=label[i])
+        plot_results(repeats=data[result_file], label=label[i], optimal=optimal)
         i += 1
 
 
@@ -63,7 +71,7 @@ if __name__ == '__main__':
               Q=Q_var, consensus_lr=[0.3], label=labels, quantization_func=['top'])
 
     plt.xlabel('epochs')
-    plt.ylabel('loss (log)')
+    plt.ylabel('Sub-Optimality (log)')
     plt.grid(axis='both')
     plt.legend()
     plt.show()
